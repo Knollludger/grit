d4fdd9d6540fa9703bc8297049fe2c9eb0ef5947
Sean Timm <seantimm3@aol.com>
2021-02-07 13:43:25 -0700

    working on file system.

--- src/file_system.rs
+++ src/file_system.rs
@@ -1,6 +1,15 @@
 use std::env;
 use std::fs;
 
+// intended functions atm:
+//      - read_file -> read in entire file.
+//      - write_file -> write an entire file.
+//      - read_line -> read in line from a file.
+//      - write_line -> read in entire file.
+//      - make_temp_dir -> make a temporary directory. Potentially copy in files.
+
+
+
 fn read_file(filename){
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
